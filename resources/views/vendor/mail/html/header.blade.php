021d4de553da5cd4c5b80a3163f858d2989e38fe
rizheez <rezamuhammad980@gmail.com>
2025-12-13 18:18:25 +0800

    feat: Add vendor mail header Blade component with dynamic logo and URL.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,9 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ asset('assets/images/logo_unu.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-height: 80px; width: auto;">
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ url('assets/images/logo_unu.png') }}" class="logo" alt="{{ config('app.name') }}"
+                style="max-height: 80px; width: auto;">
+        </a>
+    </td>
 </tr>
