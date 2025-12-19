cde503e79ecbaab82eb2b92cadc1cce927cd0cc9
rizheez <rezamuhammad980@gmail.com>
2025-12-19 21:29:44 +0800

    edit sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,45 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
-
 class SitemapController extends Controller
 {
     public function index()
     {
         $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
         $sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-        
+
         // Landing Page
         $sitemap .= '<url>';
-        $sitemap .= '<loc>' . url('/') . '</loc>';
-        $sitemap .= '<lastmod>' . now()->toAtomString() . '</lastmod>';
+        $sitemap .= '<loc>'.config('app.url').'/'.'</loc>';
+        $sitemap .= '<lastmod>'.now()->toAtomString().'</lastmod>';
         $sitemap .= '<changefreq>weekly</changefreq>';
         $sitemap .= '<priority>1.0</priority>';
         $sitemap .= '</url>';
-        
+
         // Login Page
         $sitemap .= '<url>';
-        $sitemap .= '<loc>' . route('login') . '</loc>';
-        $sitemap .= '<lastmod>' . now()->toAtomString() . '</lastmod>';
+        $sitemap .= '<loc>'.route('login').'</loc>';
+        $sitemap .= '<lastmod>'.now()->toAtomString().'</lastmod>';
         $sitemap .= '<changefreq>monthly</changefreq>';
         $sitemap .= '<priority>0.8</priority>';
         $sitemap .= '</url>';
-        
+
         // Register Page
         $sitemap .= '<url>';
-        $sitemap .= '<loc>' . route('register') . '</loc>';
-        $sitemap .= '<lastmod>' . now()->toAtomString() . '</lastmod>';
+        $sitemap .= '<loc>'.route('register').'</loc>';
+        $sitemap .= '<lastmod>'.now()->toAtomString().'</lastmod>';
         $sitemap .= '<changefreq>monthly</changefreq>';
         $sitemap .= '<priority>0.8</priority>';
         $sitemap .= '</url>';
-        
+
         $sitemap .= '</urlset>';
-        
+
         return response($sitemap, 200)
             ->header('Content-Type', 'application/xml');
     }
-    
+
     public function robots()
     {
         $robots = "User-agent: *\n";
@@ -48,8 +46,8 @@ class SitemapController extends Controller
         $robots .= "Disallow: /admin/\n";
         $robots .= "Disallow: /student/\n";
         $robots .= "\n";
-        $robots .= "Sitemap: " . url('/sitemap.xml') . "\n";
-        
+        $robots .= 'Sitemap: '.url('/sitemap.xml')."\n";
+
         return response($robots, 200)
             ->header('Content-Type', 'text/plain');
     }
