72fad358f416f0705e9fee884584545ea3e369cb
rizheez <rezamuhammad980@gmail.com>
2025-12-20 17:10:16 +0800

    remove /panduan in sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -33,14 +33,6 @@ class SitemapController extends Controller
         $sitemap .= '<priority>0.8</priority>';
         $sitemap .= '</url>';
 
-        // Panduan Page
-        $sitemap .= '<url>';
-        $sitemap .= '<loc>'.route('guide').'</loc>';
-        $sitemap .= '<lastmod>'.now()->toAtomString().'</lastmod>';
-        $sitemap .= '<changefreq>monthly</changefreq>';
-        $sitemap .= '<priority>0.7</priority>';
-        $sitemap .= '</url>';
-
         // Panduan Lengkap Page (Digital Guide)
         $sitemap .= '<url>';
         $sitemap .= '<loc>'.route('guide.view').'</loc>';
